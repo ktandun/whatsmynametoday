c69b82d27b8de41de1ff917909e26c7ae57d8881
Kenzie Tandun <nudnateiznek@gmail.com>
2025-05-28 17:00:20 +1200

    attempt to fix day of year timeozne

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 const names = ["Paula", "Pau", "Nica", "Polnics"];
 
 function seededRandom(s) {
-    return function () {
-        s = Math.sin(s) * 10000;
-        return s - Math.floor(s);
-    };
+  return function () {
+    s = Math.sin(s) * 10000;
+    return s - Math.floor(s);
+  };
 }
 
 function getTodayName() {
-    let min = 0;
-    let max = names.length - 1;
+  let min = 0;
+  let max = names.length - 1;
 
-    let rand = seededRandom(daysIntoYear(new Date()));
-    // rand = seededRandom(126);
-    let randomIndex = Math.floor(min + rand() * (max - min + 1));
+  let rand = seededRandom(daysIntoYear(new Date()));
+  // rand = seededRandom(126);
+  let randomIndex = Math.floor(min + rand() * (max - min + 1));
 
-    return names[randomIndex];
+  return names[randomIndex];
 }
 
 function daysIntoYear(date) {
-    return (
-        (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) -
-            Date.UTC(date.getFullYear(), 0, 0)) /
-        24 /
-        60 /
-        60 /
-        1000
-    );
+  const start = new Date(date.getFullYear(), 0, 0); // Jan 0 = Dec 31 previous year
+  const diff =
+    date -
+    start +
+    (start.getTimezoneOffset() - date.getTimezoneOffset()) * 60 * 1000;
+  const oneDay = 1000 * 60 * 60 * 24;
+  return Math.floor(diff / oneDay);
 }
 
 document.getElementById("name").innerHTML = getTodayName();
